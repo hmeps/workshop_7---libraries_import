0a49e99b5122cb282419b04ba8a6fca5c5b21b46
hmeps <hm18@kent.ac.uk>
2026-02-17 12:35:45 +0000

    cowsay example with list

---
+++ partTwo_cwsy.py
@@ -0,0 +1,11 @@
+import cowsay
+# cowsay.cow("Hello Everyone")
+# cowsay.cheese("What a cool life")
+# cowsay.stimpy("Who even am i?")
+cowsay.tux("What am i wearing?")
+"""
+full list of available names!
+['beavis', 'cheese', 'cow', 'daemon', 'dragon', 'fox', 'ghostbusters', 'kitty',
+'meow', 'miki', 'milk', 'octopus', 'pig', 'stegosaurus', 'stimpy', 'trex', 
+'turkey', 'turtle', 'tux']
+"""
